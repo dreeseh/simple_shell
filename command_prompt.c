2b3cbf42c21b80466e58fec6a3d969f8b17643a9
dreeseh <1859@holbertonschool.com>
2020-08-13 17:31:34 +0000

    working except PATH

--- command_prompt.c
+++ command_prompt.c
@@ -1,9 +1,8 @@
 #include "shell.h"
 #define BUFFER 255
 
-int main(void)
+char* _command_prompt(void)
 {
-
 
 	char input[BUFFER + 1] = { 0x0 };
 	char *input_pointer = input;
@@ -13,6 +12,7 @@ int main(void)
 	_prompt();
 	getline(&input_pointer, &length, stream);
 	input[_strlen(input) - 1] = '\0';
+	/* execvp(input, (char *[]){input, NULL}); */
+	return (input_pointer);
 
-	execvp(input, (char *[]){input, NULL});
 }
