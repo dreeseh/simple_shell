fcdf52f6a53b15ef8701a2c19999895139e3fd7d
dreeseh <1859@holbertonschool.com>
2020-08-19 14:59:47 +0000

    betty fixes

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #define BUFFER 255
 #include "shell.h"
 /**
- *
- *
- *
+ * main - entry point
+ * @argc: int pointer to arg count
+ * @argv: char pointer to arrays
+ * Return: on sucess
  */
 int main(int argc, char *argv[])
 {
 	pid_t pid, wpid;
 	int status;
 	unsigned int i = 0;
-
 	char cmd[100];
 	char *arg[MAXARG + 1] = {NULL};
 	char input[BUFFER + 1] = {0x0};
@@ -20,38 +20,35 @@ int main(int argc, char *argv[])
 	size_t length = BUFFER;
 	int ret_val, endoffile;
 
-
-/*	char *env[] = { (char *) "PATH=/bin", 0 }; */
 	while (1)
 	{
 		manual_mode(argc, argv);
 		_prompt();
 		endoffile = getline(&input_pointer, &length, stdin);
 		if (endoffile == EOF)
-		{
 			exit(EXIT_SUCCESS);
-		}
-
 		input[_strlen(input) - 1] = '\0';
-
 		(void)wpid;
-
-       		while (i < sizeof(arg) && *p)
+		while (i < sizeof(arg) && *p)
 		{
-			if (*p == ' ') continue;
-			if (*p == '\n') break;
-			for (arg[i++] = p ; *p && *p != ' ' && *p != '\n' ; p++);
-			*p = '\0';
-			p++;
+			if (*p == ' ')
+				continue;
+			if (*p == '\n')
+				break;
+			for (arg[i++] = p ; *p && *p != ' ' && *p != '\n' ; p++)
+			{
+				*p = '\0';
+				p++;
+			}
 		}
-
 		pid = fork();
-
 		/* child process */
 		if (pid == 0)
 		{
-			if (_strcmp (*arg, "exit") == 2) break;
-			if (_strcmp (*arg, NULL) == 2) continue;
+			if (_strcmp(*arg, "exit") == 2)
+				break;
+			if (_strcmp(*arg, NULL) == 2)
+				continue;
 			_strcpy(cmd, "/bin/");
 			_strcat(cmd, *arg);
 			if (execve(cmd, arg, NULL) == -1)
@@ -64,16 +61,14 @@ int main(int argc, char *argv[])
 		/* error forking */
 		else if (pid < 0)
 			perror("err with child process");
-
 		/*  parent process */
 		else
 		{
-			do
-			{
+			do {
 				wpid = waitpid(pid, &status, WUNTRACED);
-			}
-			while (!WIFEXITED(status) && !WIFSIGNALED(status));
-			if (_strcmp (*arg, "exit") == 2) break;
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			if (_strcmp(*arg, "exit") == 2)
+				break;
 			ret_val = 0;
 		}
 	}
